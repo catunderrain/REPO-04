9e7d781642ca67d1fbcac668dd1862ce4b2130ac
Noire <thaitoanvn1349@gmail.com>
2022-07-30 12:58:32 +0700

    oke

---
+++ gc_primary/workspace_alpha/sources/py_calendar/main.py
@@ -0,0 +1,17 @@
+from gcpaths import *
+from datetime import date
+
+year = {}
+n = date.today().year
+# year[n] = open(f'{patha}\\samples\\years\\{n}.txt', 'a')
+# year[n].close()
+
+year[n] = open(f'{patha}\\samples\\years\\{n}.txt', 'r')
+lines = year[n].readlines()
+row = []
+for line in lines:
+    row.append(line.strip().split('.'))
+
+print('m\d 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1')
+for i in range(12):
+    print(' '.join(row[i]))
